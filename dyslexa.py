0f58024ef34a19110512a1e7a659ae5fa6a2c60e
rmill <rmiller003@gmail.com>
2020-12-15 19:34:35 -0500

    Starting over

---
+++ dyslexa.py
@@ -0,0 +1,17 @@
+# This is a Speech recognition Virtual assistant application using Google AI Voice modules
+# Written by Robert Miller in Python 3.9
+
+# pip install pyaudio
+# pip install SpeechRecognition
+# pip install wikipedia
+# pip install pywhatkit
+# pip install pyjokes
+# pip install pyttsx3
+# pip install datetime
+
+import speech_recognition as sr
+import datetime
+import wikipedia
+import pyttsx3
+import pywhatkit
+import pyjokes
